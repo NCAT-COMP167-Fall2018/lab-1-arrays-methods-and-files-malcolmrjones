0af0663d5e9d4eb3a66e37ef92a2d4ccc4d6da06
Malcolm R. Jones <maljonesdev@gmail.com>
2018-08-20 17:31:19 -0400

    work

--- PlayerStats/src/playerstats/PlayerStats.java
+++ PlayerStats/src/playerstats/PlayerStats.java
@@ -1,5 +1,10 @@
 package playerstats;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author mrjones4
@@ -10,13 +15,54 @@ public class PlayerStats {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String[] names = new String[80];
+        int[] scores = new int[80];
+        
+        readPlayerNames(names, scores, args[0]);
+        
+        System.out.println(calculateAverage(scores));
+        
     }
     
     public static void readPlayerNames(String[] names, int[] scores, String filename) {
         
+        try {
+            Scanner reader = new Scanner(new File(filename));
+            
+            int currentIndex = 0;
+            while(reader.hasNext()) {
+                String[] line = reader.nextLine().split(" ");
+                
+                names[currentIndex] = line[0];
+                scores[currentIndex] = Integer.parseInt(line[1]);
+                
+                currentIndex += 1;
+            }
+            
+            
+        }
+        catch(FileNotFoundException ex) {
+            System.err.println("Unable to read file in readPlayerNames() : " + filename);
+        }
         
         
+        
+    }
+    
+    public static void displayPlayerStats(String[] names, int[] scores) {
+        for(int i = 0; i < names.length; i++) {
+            System.out.println(String.format("%s %d", names[i], scores[i]));
+        }
+    }
+    
+    public static double calculateAverage(int[] scores) {
+        double sum = 0;
+        
+        for(int i = 0; i < scores.length; i++) {
+            sum += scores[i];
+        }
+        
+        return sum / scores.length;
     }
     
 }
